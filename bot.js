1c027402bb92f86729aad3a2ce550d69fa40b565
Aiden Lammert <aiden.lammert@gmail.com>
2020-04-08 00:05:57 -0400

    init update 2

--- bot.js
+++ bot.js
@@ -1,29 +1,8 @@
-var Discord = require('discord.io');
-var auth = require('auth.json');
+const Discord = require('discord.io');
+const client = new Discord.client();
 
-// Initialize bot
-var bot = new Discord.client({
-    token : auth.token,
-    autorun : true
+client.once('ready', () => {
+    console.log('Bot ready');
 });
 
-bot.on('message', function(user, userID, channelID, message, evt){
-    if(message.substring(0,1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-
-        args = args.splice(1);
-
-        switch(cmd) {
-            case 'roles':
-                channelID.send('The available roles are:\n' +
-                    'Scrubs: !scrub\n' +
-                    'God-tier Gamers: !gamer\n' +
-                    'Overwatch Player: !overwatch\n' +
-                    'Paladins Player: !paladins\n' +
-                    'Minecraft Player: !minecraft\n' +
-                    'CoD Warzone Player: !warzone')
-        }
-    }
-})
-
+client.login('auth.json');
