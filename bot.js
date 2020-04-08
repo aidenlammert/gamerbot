01dac3350abf205091c2a0d0b4ddd49b6532a005
Aiden Lammert <aiden.lammert@gmail.com>
2020-04-08 00:54:36 -0400

    horrible security test part 2

--- bot.js
+++ bot.js
@@ -2,11 +2,11 @@ const Discord = require('discord.io');
 const auth = require('./auth.json');
 const client = new Discord.Client();
 
-gamerbot.once('ready', () => {
+client.once('ready', () => {
     console.log('Bot is ready, logged in as ${client.user.tag}');
 });
 
-gamerbot.on('message', message => {
+client.on('message', message => {
     if(message.content === '!ping') {
         message.channel.send('pong');
     }
