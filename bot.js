1115b81cb5b849913d4aa326265cdb5578afced0
Aiden Lammert <aiden.lammert@gmail.com>
2020-04-08 00:53:45 -0400

    horrible security testing time

--- bot.js
+++ bot.js
@@ -1,12 +1,10 @@
 const Discord = require('discord.io');
 const auth = require('./auth.json');
-const gamerbot = new Discord.Client();
+const client = new Discord.Client();
 
 gamerbot.once('ready', () => {
-    console.log('Bot is ready');
+    console.log('Bot is ready, logged in as ${client.user.tag}');
 });
-
-gamerbot.login(auth.token);
 
 gamerbot.on('message', message => {
     if(message.content === '!ping') {
@@ -14,4 +12,4 @@ gamerbot.on('message', message => {
     }
 });
 
-
+client.login('Njk3MjE3NzcyODQ5MjAxMjcz.Xo0Npg.jmT9gWBh1ifZqVdXo-wOMBhmANk')
