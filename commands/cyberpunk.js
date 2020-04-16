e1e77a74ec0bd6f89f8a9a74163c8e63bb33a783
Aiden Lammert <aiden.lammert@gmail.com>
2020-04-16 16:58:10 -0400

    added XCOM release checker, updated cyberpunk checker

--- commands/cyberpunk.js
+++ commands/cyberpunk.js
@@ -3,10 +3,10 @@ module.exports = {
     description: 'Check how many days until Cyberpunk 2077 releases',
 
     execute(message, args) {
-        var cyberpunk = new Date("09/17/2020");
+        var gameDate = new Date("09/17/2020");
         var today = new Date();
 
-        var difTime = cyberpunk.getTime() - today.getTime();
+        var difTime = gameDate.getTime() - today.getTime();
         var difDays = Math.floor(difTime / (1000 * 3600 * 24));
 
         message.channel.send(`There are ${difDays} days until Cyberpunk 2077`);
