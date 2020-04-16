72532ec25ca5c105af4f35f4edcf3cce4c2d7f4e
Aiden Lammert <aiden.lammert@gmail.com>
2020-04-16 16:04:34 -0400

    fixed date units

--- commands/cyberpunk.js
+++ commands/cyberpunk.js
@@ -6,8 +6,9 @@ module.exports = {
         var cyberpunk = new Date("09/17/2020");
         var today = new Date();
 
-        var dif = cyberpunk.getTime() - today.getTime();
+        var difTime = cyberpunk.getTime() - today.getTime();
+        var difDays = difTime / (1000 * 3600 * 24);
 
-        message.channel.send(`there are ${dif} days until Cyberpunk 2077`);
+        message.channel.send(`there are ${difDays} days until Cyberpunk 2077`);
     }
 }
