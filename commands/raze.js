30e947843a07205b0171b8d0c877bf62efad6b93
Aiden Lammert <aiden.lammert@gmail.com>
2020-07-11 18:23:44 -0400

    Create raze.js

---
+++ commands/raze.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'raze',
+    description: 'display Valorant copypasta', 
+
+    execute(message, args) {
+        message.channel.send(`To be clear, in VALORANT, shooting matters. You don’t kill with abilities. Abilities create tactical opportunities to take the right shot. Characters have abilities that augment their gunplay, instead of fighting directly with their abilities.`);
+    }
+}
