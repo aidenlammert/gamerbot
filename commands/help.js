af012bb86529f9f9c888f0ae81cd9a08ce6229b1
Aiden Lammert <aiden.lammert@gmail.com>
2020-09-28 20:02:46 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,24 +5,24 @@ module.exports = {
     execute(message, args) {
         message.channel.send(`My commands are:\n
     -- General --
-    !help - list commands
-    !ping - test connectivity
-    !alumni - mark yourself as UMaine Alumni
-    !guest - mark yourself as an Honorary Black Bear\n
-    -- Game Release Check --
-    !cyberpunk - Cyberpunk 2077\n
-    -- Game Role add/remove --
-    !overwatch
-    !paladins
-    !minecraft
-    !valorant
-    !turnip
-    !cod
-    !dnd
-    !ror
-    !hyperscape
-    !gtfo
-    !amongus
-    !wow`);
+!help - list commands
+!ping - test connectivity
+!alumni - mark yourself as UMaine Alumni
+!guest - mark yourself as an Honorary Black Bear\n
+-- Game Release Check --
+!cyberpunk - Cyberpunk 2077\n
+-- Game Role add/remove --
+!overwatch
+!paladins
+!minecraft
+!valorant
+!turnip
+!cod
+!dnd
+!ror
+!hyperscape
+!gtfo
+!amongus
+!wow`);
     }
 }
